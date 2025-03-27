7f70d01f16b4ad8583b77f7104b0d778f63099d3
251502 Dłubak Jakub <251502@edu.p.lodz.pl>
2025-03-27 12:39:11 +0100

    own branch creation

--- DESX/View/src/main/java/cryptodesx/view/MenuController.java
+++ DESX/View/src/main/java/cryptodesx/view/MenuController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 public class MenuController {
     @FXML
     private Label welcomeText;
-
+    // branch created
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
