ad1fbc371f8beeaed759fc1eed9986f870477913
Paweł Rajnert <pawelr6@onet.pl>
2025-04-06 14:02:17 +0200

    key splitter fix

--- DESX/Model/src/main/java/cryptoDESX/DES.java
+++ DESX/Model/src/main/java/cryptoDESX/DES.java
@@ -139,11 +139,13 @@ public class DES {
         }
 
         byte[] pc1OnKey = new byte[8];
-        int pom = 0;
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 7; j++) {
-                int bitIndex = permutation.PC1[i][j] - 1; // pobieramy numer bitu
+                int bitIndex = permutation.PC1[i][j] - 1; // pobieramy numer bitu (-1, bo indeksujemy od 0)
+                // jest to bit docelowy poniewaz permutacja PC1 mowi nam gdzie na ktore miejsce ma trafic bit z klucza np.
+                // np. 1 wartosc w macierzy PC1 to 57, zatem 1 bit z naszego klucza ma trafic na miejsce 57
+
                 int byteIndex = bitIndex / 8; // informacja o tym w ktorym bajcie jest nasz obecnie rozwazany bit
                 int bitPossition = 7 - (bitIndex % 8); // numer bitu w wyzej sprawdzanym bajcie
 
@@ -157,7 +159,6 @@ public class DES {
                 } else {
                     pc1OnKey[i] &= ~(1 << outBitIndex);
                 }
-                pom++;
             }
         }
         mainKey = pc1OnKey;
@@ -165,36 +166,43 @@ public class DES {
 
     // dzielimy 56 bitowy klucz na 2 x 28 bit podklucze
     public void mainKey56bitSplitter() {
+        // klucz powinien miec 56 bitow ALE ZAPISUJEMY TO NA 8 BAJTACH I IGNORUJEMY BIT PARZYSTOŚCI, dlatego sprawdzamy czy ma 8 bajtow
         if (mainKey.length != 8 || mainKey == null) {
             int pom = mainKey.length;
             throw new IllegalArgumentException("Klucz nie ma 8 bajtów, tylko ma ich: " + pom);
         }
+
+        byte[] left = new byte[4];
+        byte[] right = new byte[4];
+
         for (int i = 0; i < 28; i++) { // dodajemy pierwsze 28 bit do lewej części klucza
-            int byteIndex = i / 8; // rozwazany bajt
-            int bitPossition = 7 - (i % 8); // pozycja bitu w bajcie
+            int byteIndex = i / 7; // rozwazany bajt (patrzymy gdzie trafi bit, 7 bitow w bajcie rozwazamy)
+            int bitPossition = 6 - (i % 7); // pozycja bitu w bajcie
 
             // odczytujemy wartosc bitu
             boolean currentBit = ((mainKey[byteIndex] >> bitPossition) & 1) == 1;
 
             if (currentBit == true) { // jesli bit to 1 to tak go ustawiamy
-                leftKeyPart[byteIndex] |= (1 << bitPossition);
+                left[byteIndex] |= (1 << bitPossition);
             } else { // jesli bit to 0 to zostawiamy go jako 0
-                leftKeyPart[byteIndex] &= ~(1 << bitPossition);
+                left[byteIndex] &= ~(1 << bitPossition);
             }
         }
         for (int j = 28; j < 56; j++) { // dodajemy drugie 28 bit do prawej części klucza
-            int byteIndex = (j / 8); // rozwazany bajt
-            int bitPossition = 7 - (j % 8); // pozycja bitu w bajcie
+            int byteIndex = (j - 28) / 7; // rozwazany bajt w prawej części klucza
+            int bitPossition = 6 - ((j - 28) % 7); // pozycja bitu w bajcie
 
             // odczytujemy wartosc bitu
-            boolean currentBit = ((mainKey[byteIndex] >> bitPossition) & 1) == 1;
+            boolean currentBit = ((mainKey[j / 7] >> bitPossition) & 1) == 1;
 
             if (currentBit == true) { // jesli bit to 1 to tak go ustawiamy
-                rightKeyPart[byteIndex - 3] |= (1 << bitPossition);
+                right[byteIndex] |= (1 << bitPossition);
             } else { // jesli bit to 0 to zostawiamy go jako 0
-                rightKeyPart[byteIndex - 3] &= ~(1 << bitPossition);
+                right[byteIndex] &= ~(1 << bitPossition);
             }
         }
+        this.leftKeyPart = left;
+        this.rightKeyPart = right;
     }
 
     // tworzymy 16 kluczy dla kazdej z rund
@@ -202,7 +210,7 @@ public class DES {
         if (leftKeyPart.length != 4 || rightKeyPart.length != 4) {
             int pomL = leftKeyPart.length;
             int pomR = rightKeyPart.length;
-            throw new IllegalArgumentException("Podklucze nie mają po 28 bit, tylko mają kolejno, lewy: " + pomL + " prawy: " + pomR);
+            throw new IllegalArgumentException("Podklucze nie mają po 4 bajty (28 bit zapisujemy, ale potrzebujemy 32 do utworzenia pełnego bajta), tylko mają kolejno, lewy: " + pomL + " prawy: " + pomR);
         }
 
         final int[] shiftsReader = Permutation.getShifts();
@@ -258,11 +266,11 @@ public class DES {
         }
     }
 
-    public void doPC2OnRoundKeys() { // permutacja działa jak PC1, tylko tym razem robimy z 56 bitów 48
+    public void doPC2OnRoundKeys() { // permutacja działa jak PC1, tylko tym razem robimy z 56 bitów 48 (DALEJ ZAPISUJEMY TO NA 8 BAJTACH, ALE KORZYSTAMY TYLKO Z 6 BITOW NA BAJT)
         for (int roundKey = 0; roundKey < 16; roundKey++) {
             if (roundKeys[roundKey] == null || roundKeys[roundKey].length != 8) {
                 int pom = roundKeys[roundKey].length;
-                throw new IllegalArgumentException("Klucz rundowy na wejściu nie ma 56 bitów, tylko ma ich: " + pom);
+                throw new IllegalArgumentException("Klucz rundowy na wejściu nie ma 8 bajtów, tylko ma ich: " + pom);
             }
 
             byte[] pc2OnKey = new byte[6];
@@ -300,8 +308,14 @@ Jak działa algorytm DES? (zmiany w działaniu DESX opisane będą w pliku klasy
 2) tekst jawny poddajemy initial permutation
 3) tekst jawny dzielimy na pół (64 bit -> 2 x 32 bit)
 4) klucz główny poddajemy permutacji PC1, dzięki czemu usuniemy też bity parzystości (64 bit -> 56 bit)
+* klucz po PC1 zapisujemy na 8 bajtach- dlaczego? bo potrzebujemy 8 bajtów ale korzystamy tylko z 7 bit na bajt
 5) klucz główny po permutacji dzielimy na pół (56 bit -> 2 x 28 bit)
+* klucz 8 bajtowy zapisujemy na dwóch połówkach po 4 bajty (znowu podobna sytuacja, zapisujemy na 4 bajtach, ale korzystamy
+  z 3 pełnych bajtów i połówki ostatniego, bo potrzebujemy 28 bit)
 6) tworzymy 16 kluczy rundowych: kazda część podklucza przesuwana jest w lewo o określoną ilość razy
+* podklucz rundowy jest zapisany na 4 bajtach, ale korzystamy z odpowiedniej ilości bitów tylko jak wyżej
 7) łączymy każdą parę przekształconych podkluczy w jeden klucz 56 bit
+* klucz rundowy jest zapisany na 8 bajtach, ale korzystamy z odpowiedniej ilości bitów tylko jak wyżej
 8) każdy klucz rundowy poddajemy permutacji PC2
+* klucz rundowy po PC2 jest zapisany na 8 bajtach, ale korzystamy z odpowiedniej ilości bitów tylko jak wyżej
  */
