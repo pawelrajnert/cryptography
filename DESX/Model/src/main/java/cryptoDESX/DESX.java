bf394fef1ed007fc78bcfc28d1d24d443787c562
JDlubak <kuba.dl@wp.pl>
2025-04-07 17:36:33 +0200

    comments for DESX

--- DESX/Model/src/main/java/cryptoDESX/DESX.java
+++ DESX/Model/src/main/java/cryptoDESX/DESX.java
@@ -2,10 +2,20 @@ package cryptoDESX;
 
 import java.io.UnsupportedEncodingException;
 
+/*
+DESX to pewnego rodzaju ulepszenie algorytmu DES. Różni się tym, że posiada on dodatkowe dwa klucze 64-bitowe.
+Procedura zaszyfrowania wiadomości wygląda następująco:
+1) Wykonaj operacje XOR pierwszego klucza z wiadomością,
+2) Wykonaj wszystkie operacje z DESa,
+3) Wykonaj operacje XOR otrzymanej zaszyfrowanej wiadomości z drugim dodatkowym kluczem.
+W ten sposób zdecydowanie trudniej jest złamać zaszyfrowane dane komuś, jeśli nie zna kluczy.
+ */
+
 public class DESX extends DES {
     private byte[] initialKey;
     private byte[] finalKey;
-    
+
+    // klucze ustawiamy podobnie jak w oryginalnym DESie
     public void setInitialKey(String initialKey) {
         try {
             this.initialKey = isKeyCorrect(initialKey).getBytes("ISO-8859-1");
@@ -30,14 +40,19 @@ public class DESX extends DES {
         return finalKey;
     }
 
+    /*
+    Operacje XOR wykonywane przed oraz po całym algorytmie.
+    W przypadku deszyfrowania, operacje te wykonujemy w odwrotnej kolejności
+     */
     public byte[] firstEncrypt(byte[] message) {
-        for (int i = 0; i < message.length; i++) {
+        for (int i = 0; i < 8; i++) {
             message[i] = (byte) (initialKey[i] ^ message[i]);
         }
         return message;
     }
+
     public byte[] finalEncrypt(byte[] message) {
-        for (int i = 0; i < message.length; i++) {
+        for (int i = 0; i < 8; i++) {
             message[i] = (byte) (finalKey[i] ^ message[i]);
         }
         return message;
